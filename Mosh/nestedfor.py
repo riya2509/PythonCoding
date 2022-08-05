0507d9385658b61d92d99cd219d5836ef48ae237
riya2509 <riyamars3@gmail.com>
2022-08-05 14:03:11 +0530

    folders

---
+++ Mosh/nestedfor.py
@@ -0,0 +1,13 @@
+#for x in range(5):
+ #   for y in range(6):
+  #      print(f'({x},{y})')
+
+##testing nested foor loops
+numbers=[5,2,5,2,2]
+for lines in range(0,2,2):
+    print('*')
+    
+
+
+
+    
