2d2320a671163f41d2de4b07143baae837f6591d
riya2509 <riyamars3@gmail.com>
2022-08-05 08:35:47 +0530

    practising for loops

---
+++ Mosh/forloops.py
@@ -0,0 +1,11 @@
+for items in ['mosh','riya','ajay','aryana']:
+    print(items)
+for num in range(5,10,3):
+    print(num)
+
+#printing total cost of all the items in the shopping cart
+prices=(10,20,30) 
+total=0
+for item in prices:
+    total+=item
+print(total)
