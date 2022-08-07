cd59282a7930763205d72beb20a137badee1439a
riya2509 <riyamars3@gmail.com>
2022-08-07 07:42:04 +0530

    nested for lops printing designs

--- Mosh/nestedfor.py
+++ Mosh/nestedfor.py
@@ -1,11 +1,16 @@
-#for x in range(5):
- #   for y in range(6):
-  #      print(f'({x},{y})')
+for x in range(5):
+    for y in range(6):
+        print(f'({x},{y})')
 
 ##testing nested foor loops
 numbers=[5,2,5,2,2]
-for lines in range(0,2,2):
-    print('*')
+for lines in numbers:
+    print('*'*lines)
+
+    ##printing L
+    numbers=[1,1,1,1,1,6]
+    for i in numbers:
+      print('*'*i)
     
 
 
